3fbba271d7586f9ff3a0e54a1876ebe4a93431ea
Tom Gegs <tomgegs@outlook.com>
2024-01-21 12:00:51 +1000

    COMMIT: Removed FileUploadForm.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
+interface ProvidersProps {
+    children: ReactNode;
+}
 
-export default function Providers({ children }) {
-  const [queryClient] = useState(() => new QueryClient());
+export default function Providers({ children }: ProvidersProps) {
+    const [queryClient] = useState(() => new QueryClient());
 
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    );
 }
