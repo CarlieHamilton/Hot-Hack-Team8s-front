259e114b7a7a14cd770f121038de8a9b8cf015aa
Tom Gegs <tomgegs@outlook.com>
2024-01-21 14:27:53 +1000

    COMMIT: UserLoginPage

--- src/app/auth-page/page.tsx
+++ src/app/auth-page/page.tsx
@@ -1,8 +1,63 @@
-export default function Page() {
+import { useState } from "react";
+// import { useRouter } from "next/router";
+// import { useCallback } from "react";
+import Link from "next/link";
 
-  return (
-    <div>
-      Hello Auth World
-    </div>
-  )
-}
+export const UserLogin = () => {
+    // const [email, setEmail] = useState("");
+    // const [password, setPassword] = useState("");
+
+    // const router = useRouter();
+
+    // const handleRoute = useCallback(() => {
+    //     router.push("/dashboard");
+    // }, [router]);
+
+    return (
+        <div className="relative mx-auto flex h-full max-w-[600px] flex-col justify-center gap-6 object-center pt-12 align-middle">
+            <h1 className="relative text-center text-3xl font-bold uppercase text-orange-600">
+                Login{" "}
+                <span className="after:absolute after:bottom-0 after:left-1/2 after:h-[5px] after:w-[100px] after:-translate-x-12 after:translate-y-3 after:bg-orange-600" />
+            </h1>
+            <form className="flex justify">
+                <input
+                    type="email"
+                    id="email"
+                    autoComplete="email"
+                    placeholder="EMAIL"
+                    // onChange={(event) => setEmail(event.target.value)}
+                />
+                <input
+                    type="password"
+                    autoComplete="current-password"
+                    id="password"
+                    placeholder="PASSWORD"
+                    // onChange={(event) => setPassword(event.target.value)}
+                />
+                <Link href="/forgotPassword" className="underline">
+                    FORGOT PASSWORD
+                </Link>
+                <Link href="/dashboard">
+                    <button className="btn btn-outline btn-primary">
+                        SIGN IN
+                    </button>
+                </Link>
+
+                <button className="btn btn-outline btn-secondary">
+                    RESISTER
+                </button>
+            </form>
+
+            {/* Social SignIn Buttons */}
+            <div className="flex text-center">
+                <p className="uppercase">OR SIGN IN WITH</p>
+                <button
+                    className="btn btn-outline btn-accent "
+                    // onClick={handleRoute}
+                >
+                    GOOGLE
+                </button>
+            </div>
+        </div>
+    );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 "use client";
 import { useImportParsedReservations } from "@/importParsedReservations";
 import { useGetReservations } from "@/useGetReservations";
-import Image from "next/image";
+import { UserLogin } from "./auth-page/page";
 
 export default function Home() {
-  const { data: reservations, isSuccess, error } = useGetReservations();
-  const { mutate: importParsedReservations } = useImportParsedReservations();
-  isSuccess && console.log(reservations);
-  console.log(reservations, 'reservations');
-  console.log(error);
+    const { data: reservations, isSuccess, error } = useGetReservations();
+    const { mutate: importParsedReservations } = useImportParsedReservations();
+    isSuccess && console.log(reservations);
+    console.log(reservations, "reservations");
+    console.log(error);
 
-  return (
-    <main className="flex flex-col flex-grow items-center justify-between p-24">
-      
-    </main>
-  );
+    return (
+        <main className="flex flex-col flex-grow items-center justify-between p-24">
+            <UserLogin />
+        </main>
+    );
 }
